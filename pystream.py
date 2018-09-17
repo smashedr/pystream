6fad58d0e6f2c7b2e7a6f7da73d48b9fa1dcbd02
Shane <shane@shanerice.me>
2018-09-17 01:15:37 -0700

    Cleaned up the code

--- pystream.py
+++ pystream.py
@@ -2,32 +2,34 @@ import os
 import sys
 import requests
 import tempfile
-import urllib.parse
 
-os.environ['TWITCH_CLIENT_ID'] = 'bsoi6ctwjmevgsovhtwhvsc31qg29o'
+CONF = {
+    'client_id': 'bsoi6ctwjmevgsovhtwhvsc31qg29o',
+    'token_url': 'https://api.twitch.tv/api/channels/{}/access_token',
+    'usher_url': 'http://usher.twitch.tv/api/channel/hls/{}.m3u8',
+}
 
 
 def open_twitch(channel):
-    token_url = 'https://api.twitch.tv/api/channels/{}/access_token'.format(
-        channel
-    )
-    headers = {'Client-ID': os.environ['TWITCH_CLIENT_ID']}
-    r = requests.get(token_url, headers=headers, timeout=10)
-    j = r.json()
-    uri = 'http://usher.twitch.tv/api/channel/hls/{}.m3u8'.format(channel)
+    token_url = CONF['token_url'].format(channel)
+    headers = {'Client-ID': CONF['client_id']}
+    token_req = requests.get(token_url, headers=headers, timeout=10)
+    j = token_req.json()
+    usher_url = CONF['usher_url'].format(channel)
     data = {
         'player': 'twitchweb',
         'token': j['token'],
         'sig': j['sig'],
-        'allow_audio_only': 'true',
+        'allow_audio_only': 'false',
         'allow_source': 'true',
         'type': 'any',
         'p': '1234',
     }
-    params = urllib.parse.urlencode(data)
-    r = requests.get('{}?{}'.format(uri, params))
-    temp_file = tempfile.NamedTemporaryFile(suffix='.m3u8', mode='w', delete=False)
-    temp_file.write(r.content.decode())
+    usher_req = requests.get(usher_url, params=data)
+    temp_file = tempfile.NamedTemporaryFile(
+        suffix=os.path.splitext(CONF['usher_url'])[1], mode='w', delete=False
+    )
+    temp_file.write(usher_req.content.decode())
     temp_file.close()
     os.startfile(temp_file.name)
 
@@ -37,10 +39,10 @@ if __name__ == '__main__':
         if len(sys.argv) > 1:
             open_twitch(os.path.basename(sys.argv[1]))
         else:
-            open_twitch(os.path.basename(input('Twitch username: ')))
+            open_twitch(os.path.basename(input('Twitch username or URL: ')))
     except (KeyboardInterrupt, SystemExit):
         sys.exit(0)
     except Exception as error:
         print('\nCaught Exception: {}'.format(error))
-        input('\n\nYou may close this window or <enter> to exit...\n')
+        input('\n\nYou may close this window or press <enter> to exit...\n')
         sys.exit(1)
--- readme.txt
+++ readme.txt
@@ -1,10 +1,12 @@
-Can be run manually and enter in username.
+Can be run manually and enter in username or URL.
 
-Can be used programmatically by passing in the username as an argument.
+Can be used programmatically by passing in the username or URL as an argument.
 
 Usage:
-pystream.exe [username]
+pystream.exe [username/URL]
 
+Example:
+pystream.exe bobross
 
 This requires Microsoft Visual C++ Redistributable or you get an error about "MSVCP140.dll".
 https://support.microsoft.com/en-us/help/2977003/the-latest-supported-visual-c-downloads
